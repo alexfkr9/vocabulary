4c6b460a915cd18151bb701ca36653d614a7bac9
alexfkr9 <alexfkr9@gmail.com>
2023-03-20 11:08:36 +0200

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import data from "./data.json";
 
 function App() {
+  // delete selected word
+  function deleteHandler(word) {
+    setCards(cards.filter((card) => card.id !== word));
+    localStorage.setItem("saveLocal", JSON.stringify(cards));
+  }
+
+  const [cards, setCards] = useState(
+    localStorage.getItem("saveLocal") !== null
+      ? JSON.parse(localStorage.getItem("saveLocal"))
+      : data
+  );
+
+  function resetStorage() {
+    localStorage.removeItem("saveLocal");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-tab">
+        <h3>Dictionary</h3>
+        <button onClick={resetStorage()}>Reset</button>
+      </div>
+      <table>
+        <tbody>
+          {cards.map((card) => {
+            return (
+              <tr className="card" key={card.id}>
+                <td>{card.id}</td>
+                <td>{card.wordInEnglish}</td>
+                <td>{card.wordInRussian}</td>
+                <td>
+                  <button
+                    onClick={() => {
+                      deleteHandler(card.id);
+                    }}
+                  >
+                    x
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
